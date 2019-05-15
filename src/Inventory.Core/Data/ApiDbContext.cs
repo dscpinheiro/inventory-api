d622d6c164ad32dbbc980cd11a944db4036ed31e
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2019-05-15 19:58:40 -0400

    Updated context to have some prepopulated data

--- src/Inventory.Core/Data/ApiDbContext.cs
+++ src/Inventory.Core/Data/ApiDbContext.cs
@@ -1,3 +1,4 @@
+using System;
 using Inventory.Core.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,6 +16,38 @@ namespace Inventory.Core.Data
             itemEntity.Property(p => p.Name).IsRequired().HasMaxLength(128);
             itemEntity.Property(p => p.Description).HasMaxLength(512);
             itemEntity.Property(p => p.Price).IsRequired();
+
+            itemEntity.HasData(new []
+            {
+                new Item
+                {
+                    Id = Guid.Parse("64876c5f-0fc1-4a48-974e-da66d9c05630"),
+                    Name = "+5 Dexterity Vest",
+                    Description = "+5 Dexterity Vest",
+                    Price = 10
+                },
+                new Item
+                {
+                    Id = Guid.Parse("21f9427a-34b3-448f-afc4-792be2014b6e"),
+                    Name = "Elixir of the Mongoose",
+                    Description = "Elixir of the Mongoose",
+                    Price = 15
+                },
+                new Item
+                {
+                    Id = Guid.Parse("f0de704d-5d5e-4364-bd01-466f0022e8ff"),
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    Description = "Sulfuras, Hand of Ragnaros",
+                    Price = 20
+                },
+                new Item
+                {
+                    Id = Guid.Parse("09af8d3f-85bc-46a7-8d48-79ba78a01de6"),
+                    Name = "Conjured Mana Cake",
+                    Description = "Conjured Mana Cake",
+                    Price = 25
+                }
+            });
         }
     }
 }
