6d1bcffdebbd9b7fb2a7b92b08383fec7d70e526
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2019-05-16 17:30:32 -0400

    Moved IdentityServer to separate project

---
+++ src/Inventory.IdentityServer/Config.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Inventory.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources() => new List<IdentityResource>
+        {
+            new IdentityResources.OpenId()
+        };
+
+        public static IEnumerable<ApiResource> GetApis() => new List<ApiResource>
+        {
+            new ApiResource("inventoryapi", "Inventory API")
+        };
+
+        public static IEnumerable<Client> GetClients() => new List<Client>
+        {
+            new Client
+            {
+                ClientId = "authorized-client",
+
+                // no interactive user, use the clientid/secret for authentication
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                // secret for authentication
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                // scopes that client has access to
+                AllowedScopes = { "inventoryapi" }
+            }
+        };
+    }
+}
