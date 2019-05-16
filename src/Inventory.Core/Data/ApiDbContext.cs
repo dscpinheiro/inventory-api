806ae870d7f636bb882ad918659b6bd82f0c1c35
Daniel Schmidt Pinheiro <dscpinheiro@gmail.com>
2019-05-15 21:17:58 -0400

    Add quantity property to Item

--- src/Inventory.Core/Data/ApiDbContext.cs
+++ src/Inventory.Core/Data/ApiDbContext.cs
@@ -24,28 +24,32 @@ namespace Inventory.Core.Data
                     Id = Guid.Parse("64876c5f-0fc1-4a48-974e-da66d9c05630"),
                     Name = "+5 Dexterity Vest",
                     Description = "+5 Dexterity Vest",
-                    Price = 10
+                    Price = 10,
+                    Quantity = 25
                 },
                 new Item
                 {
                     Id = Guid.Parse("21f9427a-34b3-448f-afc4-792be2014b6e"),
                     Name = "Elixir of the Mongoose",
                     Description = "Elixir of the Mongoose",
-                    Price = 15
+                    Price = 15,
+                    Quantity = 20
                 },
                 new Item
                 {
                     Id = Guid.Parse("f0de704d-5d5e-4364-bd01-466f0022e8ff"),
                     Name = "Sulfuras, Hand of Ragnaros",
                     Description = "Sulfuras, Hand of Ragnaros",
-                    Price = 20
+                    Price = 20,
+                    Quantity = 15
                 },
                 new Item
                 {
                     Id = Guid.Parse("09af8d3f-85bc-46a7-8d48-79ba78a01de6"),
                     Name = "Conjured Mana Cake",
                     Description = "Conjured Mana Cake",
-                    Price = 25
+                    Price = 25,
+                    Quantity = 10
                 }
             });
         }
--- src/Inventory.Core/Models/Item.cs
+++ src/Inventory.Core/Models/Item.cs
@@ -8,5 +8,6 @@ namespace Inventory.Core.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
+        public int Quantity { get; set; }
     }
 }
